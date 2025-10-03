8bbf0da6f00f24884f174931e288c22bde5ddd83
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-10-03 00:26:25 -0600

    created Planet class

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,10 @@
-// const canvas = document.querySelector("h1");
-// console.log(canvas?.innerHTML);
-console.log("Hello world");
+const canvas = document.querySelector("canvas")?.getContext("2d");
+const h1 = document.querySelector("h1");
+console.log(h1?.innerHTML);
+// console.log("Hello world");
+let inGameplayLoop = true;
 export {};
+// while (inGameplayLoop) {
+//     console.log(`Gaming!`);
+// }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
-// const canvas = document.querySelector("h1");
+import Color from "color";
 
-// console.log(canvas?.innerHTML);
-console.log("Hello world");
+const canvas = document.querySelector("canvas")?.getContext("2d");
+const h1 = document.querySelector("h1")
+
+console.log(h1?.innerHTML);
+// console.log("Hello world");
+
+class Planet {
+
+    position: Record<("x" | "y"), number>;
+
+    radius: number;
+    mass: number;
+
+    color: typeof Color;
+
+    constructor(position:Record<("x" | "y"), number>, radius:number, mass:number, color:(typeof Color)) {
+        this.position = position;
+        this.radius = radius;
+        this.mass = mass;
+        this.color = color;
+    }
+}
