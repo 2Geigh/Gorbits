d58a9d0d6bea640a7f2a91c272f8f5cf3666d160
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-10-03 17:50:58 -0600

    added draw to screen method for planet class

--- dist/app.js
+++ dist/app.js
@@ -3,23 +3,59 @@ class Planet {
     position;
     radius;
     mass;
-    // color: ColorInstance;
-    constructor(position, radius, mass /*, color:ColorInstance*/) {
+    color;
+    constructor(position, radius, mass, color) {
         this.position = position;
         this.radius = radius;
         this.mass = mass;
-        // this.color = color;
+        this.color = color;
+    }
+    drawToScreen(ctx) {
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
     }
 }
-const canvas = document.querySelector("canvas");
-const ctx = canvas?.getContext("2d");
-const Mars = new Planet({ "x": 50, "y": 50 }, 50, 50);
-if ((ctx != null) || (ctx != undefined)) {
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.arc(Mars.position.x, Mars.position.y, Mars.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.stroke();
+class Bullet {
+    position;
+    velocity;
+    radius;
+    mass;
+    constructor(position, velocity, radius, mass) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = radius;
+        this.mass = mass;
+    }
+    updatePosition(position, velocity) {
+        position.x += velocity.x;
+        position.y += velocity.y;
+    }
 }
+// class Screen {
+//     width_pixels: number;
+//     height_pixels: number;
+//     isDarkMode: boolean;
+//     canvas: HTMLCanvasElement;
+//     constructor(width_pixels: number, height_pixels: number, isDarkMode: boolean, canvas: HTMLCanvasElement) {
+//         this.width_pixels = width_pixels;
+//         this.height_pixels = height_pixels;
+//         this.isDarkMode = isDarkMode;
+//         this.canvas = canvas;
+//     }
+// }
+const canvas = document.querySelector("canvas");
+if (canvas == null) {
+    throw new Error("No canvas element found");
+}
+const ctx = canvas?.getContext("2d");
+// const Viewport = new Screen(500, 500, false, canvas)
+const Mars = new Planet({ "x": 100, "y": 600 }, 50, 50, "red");
+if ((ctx == null) || (ctx == undefined)) {
+    throw new Error("No 2D rendering context found.");
+}
+Mars.drawToScreen(ctx);
 export {};
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,82 @@
 // import Color, { type ColorInstance } from "color";
 
+type Coordinate = Record<("x" | "y"), number>;
+
 class Planet {
-    position: Record<("x" | "y"), number>;
+    position: Coordinate;
 
     radius: number;
     mass: number;
 
-    // color: ColorInstance;
+    color: string;
 
-    constructor(position:Record<("x" | "y"), number>, radius:number, mass:number/*, color:ColorInstance*/) {
+    constructor(position:Coordinate, radius:number, mass:number, color: string) {
         this.position = position;
         this.radius = radius;
         this.mass = mass;
-        // this.color = color;
+        this.color = color;
+    }
+
+    drawToScreen(ctx: CanvasRenderingContext2D) {
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI)
+        ctx.fill();
+        ctx.stroke(); 
     }
 }
 
+class Bullet {
+    position: Coordinate;
+    velocity: Coordinate;
+    radius: number;
+    mass: number;
+
+    constructor(position: Coordinate, velocity: Coordinate, radius: number, mass: number,) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = radius;
+        this.mass = mass;
+    }
+
+    updatePosition(position: Coordinate, velocity: Coordinate) {
+        position.x += velocity.x;
+        position.y += velocity.y;
+    }
+}
+
+// class Screen {
+//     width_pixels: number;
+//     height_pixels: number;
+//     isDarkMode: boolean;
+//     canvas: HTMLCanvasElement;
+
+//     constructor(width_pixels: number, height_pixels: number, isDarkMode: boolean, canvas: HTMLCanvasElement) {
+//         this.width_pixels = width_pixels;
+//         this.height_pixels = height_pixels;
+//         this.isDarkMode = isDarkMode;
+//         this.canvas = canvas;
+//     }
+// }
+
 const canvas = document.querySelector("canvas");
+if (canvas == null) { throw new Error("No canvas element found"); }
 const ctx = canvas?.getContext("2d");
 
-const Mars = new Planet(
-                            {"x": 50, "y": 50},
-                            50,
-                            50,
-                            // Color.rgb([0,0,0])
-                        )
 
-if ((ctx != null) || (ctx != undefined)) {
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.arc(Mars.position.x, Mars.position.y, Mars.radius, 0, 2 * Math.PI)
-    ctx.fill();
-    ctx.stroke(); 
+// const Viewport = new Screen(500, 500, false, canvas)
+
+
+const Mars = new Planet(
+                            {"x": 100, "y": 600},
+                            50,
+                            50,
+                            "red"
+                        );
+
+if ((ctx == null) || (ctx == undefined)) {
+    throw new Error("No 2D rendering context found.");
 }
+
+
+Mars.drawToScreen(ctx);
