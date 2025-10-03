26a8f063fca46213251e0a9d4eb93da5f2361efa
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-10-03 00:38:34 -0600

    instantiate object of class Planet

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,25 @@
-const canvas = document.querySelector("canvas")?.getContext("2d");
-const h1 = document.querySelector("h1");
-console.log(h1?.innerHTML);
-// console.log("Hello world");
-let inGameplayLoop = true;
+// import Color, { type ColorInstance } from "color";
+class Planet {
+    position;
+    radius;
+    mass;
+    // color: ColorInstance;
+    constructor(position, radius, mass /*, color:ColorInstance*/) {
+        this.position = position;
+        this.radius = radius;
+        this.mass = mass;
+        // this.color = color;
+    }
+}
+const canvas = document.querySelector("canvas");
+const ctx = canvas?.getContext("2d");
+const Mars = new Planet({ "x": 50, "y": 50 }, 50, 50);
+if ((ctx != null) || (ctx != undefined)) {
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(Mars.position.x, Mars.position.y, Mars.radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+}
 export {};
-// while (inGameplayLoop) {
-//     console.log(`Gaming!`);
-// }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,35 @@
-import Color from "color";
-
-const canvas = document.querySelector("canvas")?.getContext("2d");
-const h1 = document.querySelector("h1")
-
-console.log(h1?.innerHTML);
-// console.log("Hello world");
+// import Color, { type ColorInstance } from "color";
 
 class Planet {
-
     position: Record<("x" | "y"), number>;
 
     radius: number;
     mass: number;
 
-    color: typeof Color;
+    // color: ColorInstance;
 
-    constructor(position:Record<("x" | "y"), number>, radius:number, mass:number, color:(typeof Color)) {
+    constructor(position:Record<("x" | "y"), number>, radius:number, mass:number/*, color:ColorInstance*/) {
         this.position = position;
         this.radius = radius;
         this.mass = mass;
-        this.color = color;
+        // this.color = color;
     }
+}
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas?.getContext("2d");
+
+const Mars = new Planet(
+                            {"x": 50, "y": 50},
+                            50,
+                            50,
+                            // Color.rgb([0,0,0])
+                        )
+
+if ((ctx != null) || (ctx != undefined)) {
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(Mars.position.x, Mars.position.y, Mars.radius, 0, 2 * Math.PI)
+    ctx.fill();
+    ctx.stroke(); 
 }
